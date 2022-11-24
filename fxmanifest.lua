d827fd20e6c7a5261fe9aaa0fcf3ddfb08528d90
DeunService <116830002+DeunService@users.noreply.github.com>
2022-11-24 21:56:39 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,7 @@ fx_version 'adamant'
 game 'gta5'
 description 'D-Logging from Deun Services'
 version '1.0'
-ui_page 'html/main.html'
 
 lua54 'yes'
 
 shared_script { 'log.lua' }
-
-
-exports {
-  'getLog'
-}
-
-server_exports {
-  'getLog'
-}
